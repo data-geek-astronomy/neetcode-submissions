0dc1c721ee776d5c6d09d850584cf4f7e0f7651d
data-geek-astronomy <149869899+data-geek-astronomy@users.noreply.github.com>
2026-06-06 18:39:48 -0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return sorted(s)==sorted(t)
+        # return Counter(s)==Counter(t)
+        if len(s)!=len(t):
+            return False
+        
+        count={}
+        for ch in s:
+            count[ch]=count.get(ch,0)+1
+
+        for ch in t:
+            if ch not in count:
+                return False
+            count[ch]-=1
+            if count[ch]<0:
+                return False
+        
+        return True
